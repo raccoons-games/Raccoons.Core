7c376603a44014f0c8dcf5fc03978c4a11b97b6c
thevitalife <vitalifelob@gmail.com>
2025-07-29 15:06:27 +0300

    RF-1 Added guid for score installer

--- Runtime/Scores/Installers/ScoreBankInstaller.cs
+++ Runtime/Scores/Installers/ScoreBankInstaller.cs
@@ -1,3 +1,4 @@
+using Raccoons.Identifiers.Guids;
 using Raccoons.Scores.Assets;
 using Raccoons.Scores.Banks;
 using Raccoons.Scores.Storages;
@@ -11,7 +12,7 @@ namespace Raccoons.Scores.Installers
 {
     public class ScoreBankInstaller : MonoInstaller
     {
-        [SerializeField] private string key;
+        [SerializeField] private GuidAsset key;
         [SerializeField] private ScoreMetadataAsset metadata;
         
         [Tooltip("Leave empty if you don't want to encrypt it")]
@@ -27,7 +28,7 @@ namespace Raccoons.Scores.Installers
                 storageChannel = new EncryptedStorageChannel(playerPrefsStorage, encryption, encryption, "S");
             }
 
-            IScoreStorage scoreStorage = new DefaultScoreStorage(key, storageChannel);
+            IScoreStorage scoreStorage = new DefaultScoreStorage(key.name, storageChannel);
             MultipliedScoreBank scoreBank = new MultipliedScoreBank(scoreStorage);
             if (metadata != null)
                 scoreBank.SetMetadata(metadata.Metadata);
